e9a7a61f227082b3a6fbc8e9fa95dd3fd153b105
3ki5tj <3ki5tj@users.noreply.github.com>
2021-09-08 10:43:47 +0000

    deploy: 4d17492c8f2550e8f9154cce8afa9afffaba7dc0

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/fastpages-gen/assets/js/search-data.json', true);
+    request.open('GET', '/fpblog/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
